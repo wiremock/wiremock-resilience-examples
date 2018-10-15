6d3ded8609560772dc3c7c6a00d2e36b92baf9d9
Tom Akehurst <t.m.akehurst@googlemail.com>
2018-10-15 21:12:40 +0100

    Added breaking change fix

--- src/main/java/resilience/ProductApiClient.java
+++ src/main/java/resilience/ProductApiClient.java
@@ -1,5 +1,6 @@
 package resilience;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -20,6 +21,7 @@ public class ProductApiClient {
     public ProductApiClient() {
         httpClient = HttpClientBuilder.create().build();
         objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 
     @Value("${productsapi.baseurl}")
