a2f8e7187a25ef8d7fbbdf9cdc81958b4fae9cfb
Tom Akehurst <t.m.akehurst@googlemail.com>
2018-10-16 21:13:22 +0100

    Made caught exception more specific

--- src/main/java/resilience/ShopResource.java
+++ src/main/java/resilience/ShopResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
@@ -31,7 +32,7 @@ public class ShopResource {
         List<RecommendedProduct> recommendedations;
         try {
             recommendedations = recommendationsApiClient.getRecommendations(productId);
-        } catch (Exception e) {
+        } catch (ResourceAccessException e) {
             recommendedations = DEFAULT_RECOMMENDATIONS;
         }
 
