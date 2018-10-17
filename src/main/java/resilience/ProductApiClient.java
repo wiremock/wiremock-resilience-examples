e293406e5503a39d890fa0f79da915dba07d918d
Tom Akehurst <tom.akehurst@johnlewis.co.uk>
2018-10-17 18:53:11 +0100

    Reverted leaky bucket fix on master

--- src/main/java/resilience/ProductApiClient.java
+++ src/main/java/resilience/ProductApiClient.java
@@ -1,39 +1,28 @@
 package resilience;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 @Component
 public class ProductApiClient {
 
-    private final HttpClient httpClient;
-    private final ObjectMapper objectMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
-    public ProductApiClient() {
-        httpClient = HttpClientBuilder.create().build();
-        objectMapper = new ObjectMapper();
-    }
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Value("${productsapi.baseurl}")
     String baseUrl;
 
     public ProductDetails getProduct(String productId) {
+        String productJson = restTemplate.getForObject(baseUrl + "/products/" + productId, String.class);
         try {
-            String url = baseUrl + "/products/" + productId;
-            HttpResponse httpResponse = httpClient.execute(new HttpGet(url));
-            if (httpResponse.getStatusLine().getStatusCode() == 502) {
-                System.err.println("Oops!"); // Don't try this at home. This is the worst possible way to handle a real error!
-                return null;
-            }
-            return objectMapper.readValue(EntityUtils.toByteArray(httpResponse.getEntity()), ProductDetails.class);
+            return objectMapper.readValue(productJson, ProductDetails.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
