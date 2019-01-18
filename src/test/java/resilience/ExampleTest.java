acb117fb6748d96907dc3fa3763bd07f56322837
Tom Akehurst <tom@wiremock.org>
2019-01-18 19:56:03 +0000

    Made example test case a bit clearer

--- src/test/java/resilience/ExampleTest.java
+++ src/test/java/resilience/ExampleTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
@@ -35,6 +36,7 @@ public class ExampleTest extends TestBase {
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("html.body.h1", is("Mac 'n Cheese Order Button"));
+                .body("html.body.h1", is("Mac 'n Cheese Order Button"))
+                .body("html.body.div.find { it.@id == 'recommendations' }.strong", is("Waffle Iron"));
     }
 }
